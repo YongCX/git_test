1bd78639041f7be52784114d5f974513893f5ef7
ycx <iscxyong@163.com>
2022-06-02 19:11:28 +0800

    idea git second commit

--- src/main/java/com/neu/test/GitTest.java
+++ src/main/java/com/neu/test/GitTest.java
@@ -9,5 +9,6 @@ package com.neu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
