57f36e6e95cb4c4835f7729bc2f61e9f7cde70fe
ycx <iscxyong@163.com>
2022-06-02 18:55:58 +0800

    idea git first commit

---
+++ src/main/java/com/neu/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.neu.test;
+
+/**
+ * @author ycx
+ * @Title: GitTest
+ * @date 2022/6/2/18:52
+ * @Description: TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
